8d9423783cce1b02d2a0324ff1d342dac92b27b4
John Bonapos <johnbonapos@outlook.com>
2022-04-19 20:07:14 +0100

    continous rendition of HTML structure to match design

--- src/main/java/com/qa/cake/CakeApplication.java
+++ src/main/java/com/qa/cake/CakeApplication.java
@@ -1,9 +1,11 @@
 package com.qa.cake;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.qa.cake"})
 public class CakeApplication {
 
 	public static void main(String[] args) {
